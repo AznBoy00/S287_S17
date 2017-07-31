40115a3c9d06d5dd85cb7fef3b56dcf0edd02c52
Kevin <Lin_Kevin_1995@hotmail.com>
2017-07-31 12:44:07 -0400

    Done Q5

--- A2/main.js
+++ A2/main.js
@@ -1,5 +1,28 @@
 function searchList() {
-	var search = document.forms[""]
-	document.getElementById("courselist").style.backgroundColor = "yellow";
-	return false;
+	var searchForm = document.forms[0]["search"].value;
+	var searchList = document.getElementById("courselist").children;
+	
+	for (var i = 0; i < document.getElementById("courselist").children.length; i++) {
+		document.getElementById("courselist").children[i].style.backgroundColor = "white";
+	}	
+	
+	if (searchForm == "") {
+		document.getElementById("search").focus();
+		alert("Please enter a course name.");
+		return false;
+	}
+	
+	document.getElementById("a").innerHTML = "Searched: " + searchForm;
+	for (var i = 0; i < document.getElementById("courselist").children.length; i++) {
+		if (document.getElementById("courselist").children[i].textContent == searchForm) {
+			document.getElementById("courselist").children[i].style.backgroundColor = "yellow";
+			return false;
+		}
+	}
+	var node = document.createElement("div");
+	var textnode = document.createTextNode(searchForm);
+	node.appendChild(textnode);
+	document.getElementById("courselist").appendChild(node);
+	
+	return false;	
 }
