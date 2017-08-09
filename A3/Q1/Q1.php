6df05503f2940a980469aeece6d45b63bfb10c5d
Kevin Lin <Lin_Kevin_1995@hotmail.com>
2017-08-09 00:31:35 -0400

    Barebone of A3

---
+++ A3/Q1/Q1.php
@@ -0,0 +1,53 @@
+<?php
+
+/* 
+ * SOEN 287 - Summer 2017
+ * Assignment 3 - Question 1
+ * Kevin Lin
+ * Bilal Rana
+ */
+
+?>
+
+<html>
+    <head>
+        <title>A3 - Q1</title>
+    </head>
+    <body>
+        <h1>
+            <?php
+
+            ?>
+        </h1>
+        <h2>
+            <?php
+
+            ?>
+        </h2>
+        <h3>
+            <?php
+
+            ?>
+        </h3>
+        <h4>
+            <?php
+
+            ?>
+        </h4>
+        <h5>
+            <?php
+
+            ?>
+        </h5>
+        <h6>
+            <?php
+
+            ?>
+        </h6>
+        <h7>
+            <?php
+
+            ?>
+        </h7>
+    </body>
+</html>
