36aadc23cadec6e64d8c0e384b7625cfa2654d06
Kevin Lin <Lin_Kevin_1995@hotmail.com>
2017-08-10 00:31:59 -0400

    Q1 Done

--- A3/Q1/Q1.php
+++ A3/Q1/Q1.php
@@ -15,6 +15,7 @@
     </head>
     <body>
         <h1>
+            factorial(3): <br /><br />
             <?php
                 echo factorial(3);
                 function factorial($number) {
@@ -30,6 +31,7 @@
         </h1>
         <br />
         <h2>
+            mostFrequent(array("tesT", "tEst", "TesT", "aSd", "Qwe", "zxc", "zxc", "zxc", "zxc")): <br /><br />
             <?php
                 echo mostFrequent(array("tesT", "tEst", "TesT", "aSd", "Qwe", "zxc", "zxc", "zxc", "zxc"));
                 function mostFrequent($array) {
@@ -44,33 +46,69 @@
         </h2>
         <br />
         <h3>
+            uppercaseFirst("asd"): <br /><br />
             <?php
-
+                echo uppercaseFirst("asd");
+                function uppercaseFirst($string) {
+                    return ucwords($string);
+                }
             ?>
         </h3>
         <br />
         <h4>
+            splitSort("this sentence will repeat the word this hello"): <br /><br />
             <?php
-
+                splitSort("this sentence will repeat the word this hello");
+                function splitSort($sentence) {
+                    $sentenceArr = explode(' ', $sentence);
+                    sort($sentenceArr);
+                    for ($i = 0; $i < count($sentenceArr); $i++) {
+                        echo "$sentenceArr[$i] ";
+                    }
+                }
             ?>
         </h4>
         <br />
         <h5>
+            dayAfterTomorrow(): <br /><br />
             <?php
-
+                echo dayAfterTomorrow();
+                function dayAfterTomorrow() {
+                    $datetime = new DateTime('tomorrow');
+                    $dayAfterTomorrow = $datetime->modify('+1 day');
+                    return $dayAfterTomorrow->format('d/m/Y');
+                }
             ?>
         </h5>
         <br />
         <h6>
+            findUnique(array('1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '3', '5', '7', '9')); <br /><br />
             <?php
-
+                findUnique(array('1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '3', '5', '7', '9'));
+                function findUnique($array) {
+                    $uniqueArr = array_unique($array);
+                    for ($i = 0; $i < count($uniqueArr); $i++) {
+                        if ($i == (count($uniqueArr) - 1)) {
+                            echo "$uniqueArr[$i].";
+                        } else {
+                            echo "$uniqueArr[$i], ";
+                        }
+                    }
+                }
             ?>
         </h6>
         <br />
-        <h7>
+        <h1>
+            formatHash(array("Alex" => "30000", "Ben" => "20000", "Cyrus" => "10000"));<br /><br />
             <?php
-
+                formatHash(array("Alex" => "30000", "Ben" => "20000", "Cyrus" => "10000"));
+                function formatHash($array) {
+                    asort($array);
+                    foreach($array as $x=>$x_value) {
+                        echo "Name:" . $x . ", Salary: " . $x_value . "<br>";
+                    }
+                }
             ?>
-        </h7>
+        </h1>
     </body>
 </html>
